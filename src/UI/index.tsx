d7329ffdac9c1e9f315ee9ab5f8fff936a3d6d03
Oleksandr Basiuk <basuksasa@gmail.com>
2024-04-15 15:11:26 +0200

    setting cra

--- src/UI/index.tsx
+++
@@ -1,5 +0,0 @@
-export { ButtonBlue } from "./ButtonBlue";
-export { ButtonGrey } from "./ButtonGrey";
-export { MyTitle } from "./MyTitle";
-export { MyLogin} from "./MyLogin";
-export {Subscribe} from './Subscribe';
--- src/components/index.tsx
+++
@@ -1,9 +0,0 @@
-export { NavBar } from "./NavBar";
-export { Main } from "./Main";
-export { SectionNextDecision } from "./SectionNextDecision";
-export { SectionMultiply } from "./SectionMultiply";
-export { SectionAuthor } from "./SectionAuthor";
-export { SectionStatistic } from "./SectionStatistic";
-export { SectionSubscribe } from "./SectionSubscribe";
-export { SectionLogin } from "./SectionLogin";
-export { Footer } from "./Footer";
